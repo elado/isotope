3a54946371d9b3c830fff4bf3a12f2973337dac9
Arthur Nisnevich <arthur.nisnevich@gmail.com>
2011-07-17 20:07:57 -0600

    Update spec to use latest revision of Date.parse by Colin Snover, and formatDate should use Date.getUTCxxxx() functions

--- test/isotope_functions.js
+++ test/isotope_functions.js
@@ -10,7 +10,7 @@ function getOrdinalSuffix(n) {
 
 function formatDate(dateISO8601) {
 	var date = new Date(Date.parse(dateISO8601));
-	return getMonthName(date.getMonth())+" "+date.getDate()+getOrdinalSuffix(date.getDate())+", "+date.getFullYear();
+	return getMonthName(date.getUTCMonth())+" "+date.getUTCDate()+getOrdinalSuffix(date.getUTCDate())+", "+date.getUTCFullYear();
 }
 
 String.prototype.parenthesize = function () {
@@ -18,29 +18,44 @@ String.prototype.parenthesize = function () {
 };
 
 /**
- * Date.parse with progressive enhancement for ISO-8601, version 2
+ * Date.parse with progressive enhancement for ISO-8601, version 5
  * © 2010 Colin Snover <http://zetafleet.com>
  * Released under MIT license.
  */
 // http://zetafleet.com/blog/javascript-dateparse-for-iso-8601
-(function () {
-	var origParse = Date.parse;
-	Date.parse = function (date) {
-		var timestamp = origParse(date), minutesOffset = 0, struct;
-		if (isNaN(timestamp) && (struct = /^(\d{4}|[+\-]\d{6})-(\d{2})-(\d{2})(?:[T ](\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3,}))?)?(?:(Z)|([+\-])(\d{2})(?::?(\d{2}))?))?/.exec(date))) {
-			if (struct[8] !== 'Z') {
-				minutesOffset = +struct[10] * 60 + (+struct[11]);
-    
-				if (struct[9] === '+') {
-					minutesOffset = 0 - minutesOffset;
-				}
-			}
-			
-			if (!struct[7]) struct[7] = "0000";
+(function (Date, undefined) {
+    var origParse = Date.parse, numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+    Date.parse = function (date) {
+        var timestamp, struct, minutesOffset = 0;
 
-			timestamp = new Date(+struct[1], +struct[2] - 1, +struct[3], +struct[4], +struct[5] + minutesOffset, +struct[6], +struct[7].substr(0, 3)).getTime();
-		}
+        // ES5 §15.9.4.2 states that the string should attempt to be parsed as a Date Time String Format string
+        // before falling back to any implementation-specific date parsing, so that’s what we do, even if native
+        // implementations could be faster
+        //              1 YYYY                2 MM       3 DD           4 HH    5 mm       6 ss        7 msec        8 Z 9 ±    10 tzHH    11 tzmm
+        if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(date))) {
+            // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
+            for (var i = 0, k; (k = numericKeys[i]); ++i) {
+                struct[k] = +struct[k] || 0;
+            }
 
-		return timestamp;
-	};
-}());
+            // allow undefined days and months
+            struct[2] = (+struct[2] || 1) - 1;
+            struct[3] = +struct[3] || 1;
+
+            if (struct[8] !== 'Z' && struct[9] !== undefined) {
+                minutesOffset = struct[10] * 60 + struct[11];
+
+                if (struct[9] === '+') {
+                    minutesOffset = 0 - minutesOffset;
+                }
+            }
+
+            timestamp = Date.UTC(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]);
+        }
+        else {
+            timestamp = origParse ? origParse(date) : NaN;
+        }
+
+        return timestamp;
+    };
+}(Date));
